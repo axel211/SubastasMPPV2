ef7143debd72fdcf30ce0d8ccd07576aebed5883
axel211 <axelromero2197@outlook.com>
2024-05-10 01:07:13 -0500

    update

--- frontend/components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import logo from 'public/logoMuni/LogoMPP.svg'
-export default function Logo({ ...rest }) {
-  return (
-    <div>
-    <img src={logo} alt="Logo" className="logo" style={{ width: '250px', height: 'auto' }} />
-  </div>
-  );
-}
